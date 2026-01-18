ad236090a4205a45be9a24970bc2176dcb8465c2
EJANisMeh <ejanismeh@gmail.com>
2026-01-18 18:11:24 +0800

    Solved warnings

--- src/ServerScriptService/Combat/CombatSystem/InitCombatEventHandlers/inCombatEventHandlers.lua
+++ src/ServerScriptService/Combat/CombatSystem/InitCombatEventHandlers/inCombatEventHandlers.lua
@@ -1,29 +1,29 @@
 local ServerScriptService = game:GetService("ServerScriptService")
 local EventDispatcher = require(ServerScriptService:WaitForChild("EventDispatcher"))
 local CombatSystemModule = ServerScriptService.Combat:WaitForChild("CombatSystem")
-local InCombatSystem =  require(CombatSystemModule:WaitForChild("In-Combat"))
+local InCombatSystem = require(CombatSystemModule:WaitForChild("In-Combat"))
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local playerData = require(ReplicatedStorage.Data:WaitForChild("PlayerData"))
+-- unused port
+-- local playerData = require(ReplicatedStorage.Data:WaitForChild("PlayerData"))
 local eventsData = require(ReplicatedStorage.Data:WaitForChild("EventsData"))
 local CombatActionEvent = ReplicatedStorage.Events:WaitForChild("CombatAction")
 
-
-function regInCombatEventHandlers()
+local function regInCombatEventHandlers()
 	EventDispatcher:Register(eventsData.CombatActions.Fight, function(context, button)
 		print("Clicked:", button)
 	end)
-	
+
 	EventDispatcher:Register(eventsData.CombatActions.Escape, function(context, button)
 		local player = context.player
 		local playerId = tostring(context.player.UserId)
-		
+
 		CombatActionEvent:FireClient(player, eventsData.CombatActions.Escape)
 		EventDispatcher:Dispatch(eventsData.Connections.DisconnectInCombatConnections, tostring(playerId))
-		
+
 		InCombatSystem:DisableCombatGui(player)
 		InCombatSystem:EndCombat(player)
-		
+
 		if script.inCombatEventHandlersTest.Value then
 			print("Successfully escaped")
 		end
@@ -31,5 +31,5 @@ function regInCombatEventHandlers()
 end
 
 return {
-	regInCombatEventHandlers = regInCombatEventHandlers
+	regInCombatEventHandlers = regInCombatEventHandlers,
 }
